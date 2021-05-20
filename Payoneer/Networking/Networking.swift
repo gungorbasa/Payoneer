d2101d1b45b8976ad6de3984d6e91720c1acb898
Gungor Basa <gungorbasa@gmail.com>
2021-05-20 19:33:21 +0300

    Remove public from networking protocol

--- Payoneer/Networking/Networking.swift
+++ Payoneer/Networking/Networking.swift
@@ -7,7 +7,7 @@
 
 import Combine
 
-public protocol Networking {
+protocol Networking {
   func run<T: Decodable>(_ route: Routing, completion: @escaping (Result<T, Error>) -> Void)
   func run<T>(_ route: Routing) -> AnyPublisher<T, Error> where T : Decodable
 }
